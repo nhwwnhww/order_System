63ec9cf06f3c04c8c943f98bffe164fe5d7a52c7
nhwwnhww <nhwenhww@gmail.com>
2022-02-26 09:57:50 +1000

    main

--- order.js
+++ order.js
@@ -1,16 +1,31 @@
-function build_item(data){
-    var body = document.querySelector("#body");
+function build_item(data,section){
+    var get_main = document.querySelector("#main");
+
+    var body = document.createElement("div");
+    body.className = `container-fluid bg-warning`;
+    body.style = `display: flex;
+                    flex-wrap: wrap;`;
+    body.setAttribute('id',section);
+    get_main.appendChild(body);
+
 
     for (var i = 0; i < data.length; i++){
         // add container
         var container = document.createElement("div");
-        container.className = `container`;
+        container.className = `container mt-3`;
+        container.style = `width:20%;
+                            height:auto`;
         body.appendChild(container);
 
+        // bootstrap border
+        var container_border = document.createElement("span");
+        container_border.className = `border border-success rounded-3`;
+        container.appendChild(container_border);
+
         // container
-        var p_name = document.createElement("h1");
-        var price = document.createElement("h2");
-        var value = document.createElement("h3");
+        var p_name = document.createElement("p");
+        var price = document.createElement("p");
+        var value = document.createElement("p");
         p_name.className = `item_box${i}`;
         value.className = `box${i}`;
 
@@ -18,7 +33,7 @@ function build_item(data){
         price.innerText = '$' + data[i][1];
         value.innerText = '0';
 
-        container.append(p_name);
+        container_border.append(p_name);
         container.append(price);
         container.append(value);
 
@@ -26,8 +41,12 @@ function build_item(data){
         var add_v = document.createElement("button");
         var minus = document.createElement("button");
         
-        add_v.innerText = "+++";
-        minus.innerText = "---";
+        // classname
+        add_v.className = "btn-default btn btn-primary";
+        minus.className = "btn-default btn btn-primary";
+
+        add_v.innerText = "+";
+        minus.innerText = "-";
         var item_name = p_name.className;
         add_box = `add_value('${value.className}','${item_name}')`;
         minus_box=`minus_value('${value.className}','${item_name}')`;
@@ -53,12 +72,16 @@ function add_value(box,item){
 function minus_value(box,item){ 
     var value = document.querySelector(`.${box}`);
     var origin = parseInt(value.innerHTML);
+
+    var food = document.querySelector(`.${item}`);
+    var food_name = food.innerText;
     if (origin == 0){
         return;
     }
-    origin -= 1;
+    removeItemOnce(cart,food_name);
+    origin = origin - 1;
     value.innerText = origin;
-    delete_cart(cart,item);
+    console.log(cart);
     return;
 }
 // add to cart
@@ -68,26 +91,24 @@ function add_cart(cart,item){
     cart.push(food_name);
     console.log(cart);
 }
-// delete item from cart
-function delete_cart(cart,item){
-    var food = document.querySelector(`.${item}`);
-    var food_name = food.innerText;
-    // delete the chosen item from array
-    cart.map((val, i) => {
-        if (val === food_name) {
-            cart.splice(i,1);
-            console.log(cart);
-        }
-    })
-}
+// remove item once form array
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
 
 // display the result
 function show(){
     var result = document.getElementById("result");
     cart.sort();
+    result.innerHTML = "";
     for (var i = 0; i < cart.length;i++){
-        var show = document.createElement("h3");
-        show.innerText = cart[i];
-        result.appendChild(show);
+        var show_p = document.createElement("h3");
+        show_p.innerText = cart[i];
+        result.appendChild(show_p);
+        console.log(i);
     }
 }
